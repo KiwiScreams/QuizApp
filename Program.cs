cf81a27e872e3d26c6860b2022e5da148104a582
KiwiScreams <screamskiwi3@gmail.com>
2026-06-01 10:37:30 +0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using QuizApp.Forms;
+
+namespace QuizApp;
+
+internal static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
