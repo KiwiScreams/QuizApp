51490e6b4ad6036a9559133cf9f85b6968bf5303
KiwiScreams <screamskiwi3@gmail.com>
2026-06-01 11:48:26 +0400

    Game Session Model

---
+++ Models/GameSession.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace QuizApp.Models;
+
+public class GameSession
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int QuestionsCount { get; set; }
+    public int Score { get; set; }
+    public DateTime SessionStart { get; set; }
+    public DateTime? SessionEnd { get; set; }
+}
