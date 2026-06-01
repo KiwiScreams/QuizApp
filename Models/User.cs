4dc39b01d6c5191428203caae93bfc6194b665b7
KiwiScreams <screamskiwi3@gmail.com>
2026-06-01 10:55:24 +0400

    User Model

--- Models/User.cs
+++ Models/User.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace QuizApp.Models
+namespace QuizApp.Models;
+
+public class User
 {
-    internal class User
-    {
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime RegistrationDate { get; set; }
+    public decimal? Rank { get; set; }
 }
